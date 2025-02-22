1dac29d1155147d941a390a6289bec24566d5846
Michael Martinez De La Hoz <maicolmartinezjd@unimagdalena.edu.co>
2025-02-22 01:48:47 -0500

    ajustes de diseño v3, validacion de eliminacion al listar facturas

--- src/app/features/facturas/components/factura-list/factura-list.component.ts
+++ src/app/features/facturas/components/factura-list/factura-list.component.ts
@@ -6,6 +6,7 @@ import { HeaderComponent } from '../../../../components/header/header.component'
 import { MatIconModule } from '@angular/material/icon';
 import { NgxPaginationModule } from 'ngx-pagination';
 import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 @Component({
   selector: 'app-factura-list',
   standalone: true,
@@ -32,6 +33,32 @@ export class FacturaListComponent implements OnInit {
       this.totalItems = this.invoices.length;
     });
   }
+
+  confirmarEliminacion(id: number) {
+      console.log(id);
+      Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+      }).then((result) => {
+        if (result.isConfirmed) {
+          if(id === undefined) {
+            this.invoices = this.invoices.filter((_: any, i: number) => i !== id);
+            return;
+          }
+          this.deleteFactura(id);
+          Swal.fire({
+            title: "Deleted!",
+            text: "Your file has been deleted.",
+            icon: "success"
+          });
+        }
+      });
+    }
 
   deleteFactura(id: number) {
     this.facturaService.deleteFactura(id).subscribe(() => {
